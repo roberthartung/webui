5e433f6eab96de62e3bb1c9a197471197a85a54a
Robert Hartung <robert-hartung@web.de>
2014-10-23 16:36:20 +0200

    dock element added.

---
+++ lib/layout/webui-dock.dart
@@ -0,0 +1,10 @@
+library webui.layout.dock;
+
+import 'package:polymer/polymer.dart';
+
+@CustomTag('webui-dock')
+class WebUiDock extends PolymerElement {
+  WebUiDock.created() : super.created() {
+    
+  }
+}
